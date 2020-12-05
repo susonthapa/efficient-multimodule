ae14c177821f10160f001b76d5aef26bb14159c9
suson <susuthapa19961227@gmail.com>
2020-12-05 17:46:23 +0545

    gradle uploadArchives task added, custom dependency resolution for local modules implemented

--- app/src/main/java/np/com/susanthapa/efficientmultimodule/MainActivity.kt
+++ app/src/main/java/np/com/susanthapa/efficientmultimodule/MainActivity.kt
@@ -1,11 +1,29 @@
 package np.com.susanthapa.efficientmultimodule
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import np.com.susanthapa.efficientmultimodule.databinding.ActivityMainBinding
+import np.com.susanthapa.feature_a.FeatureAActivity
+import np.com.susanthapa.feature_b.FeatureBActivity
+import np.com.susanthapa.feature_c.FeatureCActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.featureA.setOnClickListener {
+            startActivity(Intent(this, FeatureAActivity::class.java))
+        }
+
+        binding.featureB.setOnClickListener {
+            startActivity(Intent(this, FeatureBActivity::class.java))
+        }
+
+        binding.featureC.setOnClickListener {
+            startActivity(Intent(this, FeatureCActivity::class.java))
+        }
     }
 }
